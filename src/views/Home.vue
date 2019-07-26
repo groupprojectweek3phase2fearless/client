21cba7bd91f53c63b418541a2870b0795adb2b6d
hendrixsilaen <silaen.hjr@gmail.com>
2019-07-26 13:28:58 +0700

    gabungan branch kholis + tampilan

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,53 @@
 <template>
   <div class="home">
-    <form @submit.prevent="register">
+    <!-- <form @submit.prevent="register">
       <input 
         v-model="username"
         class="create-username-input"
         type="text" 
-        placeholder="username">
+        placeholder="username"> -->
+
       <!-- <input 
         class="create-username-input"
         type="submit"> -->
-    </form>
+
+    <!-- </form> -->
+
+
+    <!-- HENDRIX -->
+    <div style="padding: 10%">
+        <div class="title-container">
+            <span style="font-size: 20pt;">Welcome to</span>
+            <br>
+            <span class="landing-title">¿ Yes or No ?</span>
+            <br>
+            <span style="font-size: 20pt;">Battleground</span>
+        </div>
+
+        <div>
+            <b-form @submit.prevent="register">
+                <b-form-group>
+                    <b-form-input
+                    id="inputName"
+                    v-model="username"
+                    type="text"
+                    placeholder= "Input your name to play . . . 😜"
+                    required
+                    >
+                    </b-form-input>
+                </b-form-group>
+
+                <b-button id="btn-submit" type="submit" variant="primary">Enter the Game</b-button>
+            </b-form>
+        </div>
+
+
+        <div class="character" id="char-1">
+            <img style="width: 100%;" src="https://mir-s3-cdn-cf.behance.net/project_modules/disp/b9cc6d42094811.57c019c3d3cab.gif" alt="">
+        </div>
+        
+    </div>
+    <!-- HENDRIX -->
   </div>
 </template>
 
@@ -45,4 +83,34 @@ export default {
     width: 200px;
     margin: 5px
   }
+
+  #inputName{
+        width: 80%;
+        margin: 0px auto 0px auto;
+        text-align: center;
+        height: 100px;
+        font-size: 25pt;
+    }
+
+    .landing-title{
+        font-size: 40pt;
+    }
+
+    .title-container{
+        margin-bottom: 50px;
+    }
+
+    .character{
+        position: fixed;
+        bottom: 0;
+        right: 0;
+        width: 20%;
+        z-index: -1;
+    }
+
+    #btn-submit{
+        width: 200px;
+        height: 50px;
+        border-radius: 20px;
+    }
 </style>
--- src/views/Lobby.vue
+++ src/views/Lobby.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="create-room">
-    Create Room
+  <div style="padding: 5%; height: 100%;">
+    <!-- Create Room
     <form @submit.prevent="createRoom">
       <input 
         class="create-room-input"
@@ -18,31 +18,68 @@
         <button v-if='lobby.players.length < 5' @click.prevent='joinRoom(lobby.id)'>Join!</button>
         <p v-if='lobby.players.length == 5'>Full</p>
       </RoomCard>
-    </div>
+    </div> -->
+
+
+    <!-- HENDRIX -->
+        <div class="title-container">
+            <span style="font-size: 20pt;">Hi, {{ playerName }} </span>
+            <br>
+            <span class="landing-title">¿ Are you ready to play ?</span>
+        </div>
+
+        <div style="height: 50%;">
+            <span style="font-size: 20pt;">Select your room or create one</span>
+            <br>
+
+            <b-button v-b-modal.modal-add-room variant="success">&#10010; Add Room</b-button>
+            
+            <div id="room-container">
+                <div v-for="(room, index) in rooms" :key="index" class="room">
+                    <div style="padding:10px;">
+                        <h3>{{ room.roomName }}</h3>
+                        Player: {{ room.players.length }} / 50
+                        <br>
+                        <b-button :disabled="room.players.length == 50" variant="success" @click="joinRoom(room.id)">Join</b-button>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <add-room @addRoom="createRoom"></add-room>
+
+
+        <div class="character" id="char-1">
+            <img style="width: 100%;" src="https://mir-s3-cdn-cf.behance.net/project_modules/disp/d85d3942094811.57c019c5360b1.gif" alt="">
+        </div>
+    <!-- HENDRIX -->
   </div>
 </template>
 
 <script>
 import RoomCard from '../components/RoomCard'
 import { mapState, mapMutations, mapActions } from 'vuex'
+import AddRoom from "../components/AddRoom";
 
 export default {
   data() {
     return {
-      roomName: ''
+      roomName: '',
+      playerName: ''
     }
   },
   components: {
-    RoomCard
+    RoomCard, AddRoom
   },
   methods: {
-    createRoom() {
-      this.$store.dispatch('createRoom', this.roomName)
-      localStorage.setItem('room', this.roomName)
-      this.roomName = ''
+    createRoom(name) {
+      this.$store.dispatch('createRoom', name)
+      localStorage.setItem('room', name)
+      // this.roomName = ''
     },
 
     joinRoom (roomId) {
+      console.log(roomId);
       this.$store.dispatch('joinRoom', roomId);
     }
   },
@@ -51,10 +88,58 @@ export default {
   },
   created() {
     this.$store.dispatch('getAllRooms')
+    this.playerName = localStorage.getItem('username');
   },
 }
 </script>
 
-<style>
+<style scoped>
+  #inputName{
+      width: 80%;
+      margin: 0px auto 0px auto;
+      text-align: center;
+      height: 100px;
+      font-size: 25pt;
+  }
 
+  .landing-title{
+      font-size: 40pt;
+  }
+
+  .title-container{
+      margin-bottom: 50px;
+  }
+
+  .character{
+      position: fixed;
+      bottom: 0;
+      right: 0;
+      width: 20%;
+  }
+
+  .character:hover{
+      display: none;
+  }
+
+  #btn-submit{
+      width: 200px;
+      height: 50px;
+      border-radius: 20px;
+  }
+
+  #room-container{
+      height: 50vh;
+      border-top-left-radius: 50px;
+      background-color: #36677c;
+      text-align: center;
+      max-width: 80%;
+      margin: 50px auto 0px auto;
+      overflow: auto;
+  }
+
+  .room{
+      margin: 5% 10%;
+      border-radius: 10px;
+      background-color: antiquewhite;
+  }
 </style>
